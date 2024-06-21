caed2a6b331486821ae96d22609d42bbc9c384eb
coutinholaura <110981345+coutinholaura@users.noreply.github.com>
2024-06-20 21:51:41 -0300

    Atualizando o conjunto de testes.py

--- codigo/testes/testes.py
+++ codigo/testes/testes.py
@@ -13,6 +13,100 @@ from algoritmos.guloso import greedy_max_value, greedy_max_value_per_mw
 from algoritmos.divisão_conquista import divide_and_conquer
 from algoritmos.programação_dinamica import dp_solution
 
+def ler_conjunto_empresas(entrada):
+    linhas = entrada.strip().split("\n")
+    lances = []
+    for linha in linhas:
+        partes = linha.split(";")
+        nome = partes[0]
+        quantidade = int(partes[1])
+        valor = int(partes[2])
+        lances.append((quantidade, valor))
+    return lances
+
+conjunto_empresas_1 = """
+E1;430;1043
+E2;428;1188
+E3;410;1565
+E4;385;1333
+E5;399;1214
+E6;382;1498
+E7;416;1540
+E8;436;1172
+E9;416;1386
+E10;423;1097
+E11;400;1463
+E12;406;1353
+E13;403;1568
+E14;390;1228
+E15;387;1542
+E16;390;1206
+E17;430;1175
+E18;397;1492
+E19;392;1293
+E20;393;1533
+E21;439;1149
+E22;403;1277
+E23;415;1624
+E24;387;1280
+E25;417;1330
+"""
+
+conjunto_empresas_2 = """
+E1;430;1043
+E2;428;1188
+E3;410;1565
+E4;385;1333
+E5;399;1214
+E6;382;1498
+E7;416;1540
+E8;436;1172
+E9;416;1386
+E10;423;1097
+E11;400;1463
+E12;406;1353
+E13;403;1568
+E14;390;1228
+E15;387;1542
+E16;390;1206
+E17;430;1175
+E18;397;1492
+E19;392;1293
+E20;393;1533
+E21;439;1149
+E22;403;1277
+E23;415;1624
+E24;387;1280
+E25;417;1330
+E1;313;1496
+E2;398;1768
+E3;240;1210
+E4;433;2327
+E5;301;1263
+E6;297;1499
+E7;232;1209
+E8;614;2342
+E9;558;2983
+E10;495;2259
+E11;310;1381
+E12;213;961
+E13;213;1115
+E14;346;1552
+E15;385;2023
+E16;240;1234
+E17;483;2828
+E18;487;2617
+E19;709;2328
+E20;358;1847
+E21;467;2038
+E22;363;2007
+E23;279;1311
+E24;589;3164
+E25;476;2480
+"""
+
+energia_disponivel = 8000
+
 def medir_tempo_algoritmos(conjuntos, energia_disponivel):
     resultados_guloso1 = []
     resultados_guloso2 = []
@@ -20,7 +114,7 @@ def medir_tempo_algoritmos(conjuntos, energia_disponivel):
     tempos_guloso2 = []
 
     for idx, lances in enumerate(conjuntos):
-        print(f"Rodando conjunto {idx + 1} com guloso")
+        print(f"Rodando conjunto {idx + 1} com greedy algorithms")
         inicio = time.time()
         resultado1 = greedy_max_value(lances, energia_disponivel)
         fim = time.time()
@@ -58,7 +152,7 @@ def medir_tempo_dc_pd(conjuntos, energia_disponivel):
     tempos_dc = []
     tempos_pd = []
 
-    for idx, lances in enumerate(conjuntos):
+    for idx, lances in conjuntos:
         print(f"Rodando conjunto {idx + 1} com divisão e conquista")
         inicio = time.time()
         resultado_dc = divide_and_conquer(lances, energia_disponivel)
@@ -77,26 +171,36 @@ def medir_tempo_dc_pd(conjuntos, energia_disponivel):
 
 
 def main():
-    tam_max = 100  # Pode ser ajustado conforme necessidade
-    incremento = 1
-    energia_disponivel = 1000
+    # Ler os conjuntos fornecidos
+    conjunto_1 = ler_conjunto_empresas(conjunto_empresas_1)
+    conjunto_2 = ler_conjunto_empresas(conjunto_empresas_2)
 
-    print("Gerando conjuntos de teste para backtracking...")
-    conjuntos_de_teste = gerar_conjuntos(tam_max, incremento)
-    tamanho_T, tempos_backtracking = medir_tempo_backtracking(conjuntos_de_teste, energia_disponivel)
-    print(f'Tamanho T encontrado para backtracking: {tamanho_T}')
+    # Executar testes para o primeiro conjunto
+    print("\nExecutando testes para o conjunto de empresas interessadas 1\n")
+    conjuntos_de_teste = [conjunto_1]
+    medir_tempo_backtracking(conjuntos_de_teste, energia_disponivel)
     
-    print("Gerando conjuntos de teste adicionais para algoritmos gulosos...")
-    tamanho_10T = 10 * tamanho_T
-    conjuntos_de_teste_guloso = gerar_conjuntos(tamanho_10T, tamanho_T)
-    
-    (resultados1, tempos1), (resultados2, tempos2) = medir_tempo_algoritmos(conjuntos_de_teste_guloso, energia_disponivel)
+    (resultados1, tempos1), (resultados2, tempos2) = medir_tempo_algoritmos(conjuntos_de_teste, energia_disponivel)
     print(f'Médias dos resultados e tempos do primeiro algoritmo guloso: {sum(resultados1)/len(resultados1)}, {sum(tempos1)/len(tempos1)}')
     print(f'Médias dos resultados e tempos do segundo algoritmo guloso: {sum(resultados2)/len(resultados2)}, {sum(tempos2)/len(tempos2)}')
 
-    (resultados_dc, tempos_dc), (resultados_pd, tempos_pd) = medir_tempo_dc_pd(conjuntos_de_teste_guloso, energia_disponivel)
-    print(f'Médias dos resultados e tempos de divisão e conquista: {sum(resultados_dc)/len(resultados_dc)}, {sum(tempos_dc)/len(tempos_dc)}')
-    print(f'Médias dos resultados e tempos de programação dinâmica: {sum(resultados_pd)/len(resultados_pd)}, {sum(tempos_pd)/len(tempos_pd)}')
+    imprimir = medir_tempo_dc_pd(conjuntos_de_teste, energia_disponivel)
+    print(f'Médias dos resultados e tempos de divisão e conquista: {sum(imprimir[0][0])/len(imprimir[0][0])}, {sum(imprimir[0][1])/len(imprimir[0][1])}')
+    print(f'Médias dos resultados e tempos de programação dinâmica: {sum(imprimir[1][0])/len(imprimir[1][0])}, {sum(imprimir[1][1])/len(imprimir[1][1])}')
+
+    # Executar testes para o segundo conjunto
+    print("\nExecutando testes para o conjunto de empresas interessadas 2\n")
+    conjuntos_de_teste = [conjunto_2]
+    medir_tempo_backtracking(conjuntos_de_teste, energia_disponivel)
+    
+    (resultados1, tempos1), (resultados2, tempos2) = medir_tempo_algoritmos(conjuntos_de_teste, energia_disponivel)
+    print(f'Médias dos resultados e tempos do primeiro algoritmo guloso: {sum(resultados1)/len(resultados1)}, {sum(tempos1)/len(tempos1)}')
+    print(f'Médias dos resultados e tempos do segundo algoritmo guloso: {sum(resultados2)/len(resultados2)}, {sum(tempos2)/len(tempos2)}')
+
+    imprimir = medir_tempo_dc_pd(conjuntos_de_teste, energia_disponivel)
+    print(f'Médias dos resultados e tempos de divisão e conquista: {sum(imprimir[0][0])/len(imprimir[0][0])}, {sum(imprimir[0][1])/len(imprimir[0][1])}')
+    print(f'Médias dos resultados e tempos de programação dinâmica: {sum(imprimir[1][0])/len(imprimir[1][0])}, {sum(imprimir[1][1])/len(imprimir[1][1])}')
+
 
 if __name__ == "__main__":
     main()
